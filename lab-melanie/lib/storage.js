2f5c21bbbdc705832dbff5d30475748ca84f5714
Melanie Downing <sayanything830@gmail.com>
2018-01-24 18:06:11 -0800

    not able to get, put, or delete

---
+++ lab-melanie/lib/storage.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const debug = require('debug')('http:storage');
+
+const storage = module.exports = {};
+const memory = {};
+
+
+storage.create = function(schema, item) {
+  debug('Created a new thing');
+
+  return new Promise((resolve, reject) => {
+    if(!schema || !item) return reject(new Error('Cannot create a new item; Schema and Item required'));
+
+    if(!memory[schema]) memory[schema] = {};
+
+    memory[schema][item._id] = item;
+    return resolve(item);
+  });
+};
+
+// storage.fetchOne = function() {
+  
+
+// };
+
+storage.fetchAll = function(schema) {
+  debug('Fetch All things');
+
+  return new Promise((resolve, reject) => {
+    if(!schema) return reject(new Error('Cannot get item, need a schema'));
+
+    return resolve(memory[schema]);
+  });
+};
+
+storage.update = function(schema, item) {
+  debug('Updating a thing');
+
+  return new Promise((resolve, reject) => {
+    if(!item._id) return reject(new Error('Cannot update, need item'));
+    if(!memory[schema][item._id]) return reject(new Error('Cannot update, need an id'));
+
+    return resolve(item);
+  });
+};
+
+storage.delete = function(schema, item) {
+  debug('Deleting a thing');
+  
+  return new Promise((resolve, reject) => {
+    if(memory[schema][item._id]) {
+      delete memory[schema][item._id];
+      return resolve();
+    }
+    return reject(new Error('Cannot delete item, item not found'));
+  });
+};
--- lab-melanie/route/route-note.js
+++ lab-melanie/route/route-note.js
@@ -3,3 +3,92 @@
 const Note = require('../model/note.js');
 const storage = require('../lib/storage.js');
 const debug = require('debug')('http:route-note');
+
+module.exports = function(router) {
+  router.post('/api/v1/note', (req, res) => {
+    debug('POST /api/v1/note')
+
+    try {
+      
+      let newNote = new Note(req.body.title, req.body.content);
+
+      storage.create('Note', newNote)
+        .then(item => {
+          res.writeHead(201, {'Content-Type': 'application/json'});
+          res.write(JSON.stringify(item));
+          res.end();
+        });
+    } catch(err) {
+      debug(`There was a bad request: ${err}`);
+
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.write('Bad Request');
+      res.end();
+    }
+  });
+
+  router.get('/api/v1/note', (req, res) => {
+    debug('GET /api/v1/note');
+    try {
+
+      storage.fetchAll('Note')
+        .then(item => {
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          res.write(JSON.stringify(item));
+          res.end();
+        });
+    } catch(err) {
+      debug(`Note not found: ${err}`);
+
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('Not Found');
+      res.end();
+    }
+   
+
+  });
+
+  router.put('/api/v1/note', (req, res) => {
+    debug(`PUT /api/v1/note`);
+
+    try {
+
+      let newNote = new Note(req.body.title, req.body.content);
+      
+      storage.update('Note', newNote)
+        .then(item => {
+          res.writeHead(204, {'Content-Type': 'application/json'});
+          res.write(JSON.stringify(item));
+          res.end();
+        });
+    } catch(err) {
+      debug(`There was a bad request: ${err}`);
+
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.write('Bad Request');
+      res.end();
+    }
+
+  });
+
+  router.delete('/api/v1/note', (req, res) => {
+    debug('DELETE /api/v1/note');
+    // debug(`query id ${req.url.query._id}`);
+
+    try {
+
+      storage.delete('Note', req.url.query._id)
+        .then(item => {
+          res.writeHead(204, {'Content-Type': 'application/json'});
+          res.write(JSON.stringify(item));
+          res.end();
+        });
+    } catch(err) {
+      debug(`There was a bad request: ${err}`);
+
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.write('Bad Request');
+      res.end();
+    }
+  });
+};
