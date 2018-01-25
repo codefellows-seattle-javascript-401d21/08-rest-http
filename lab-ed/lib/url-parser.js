a23f4c82c53a8e5127a1c5afcd949e11ad22db30
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-01-24 16:21:05 -0800

    add url parser

---
+++ lab-ed/lib/url-parser.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const urlParser = require('url')
+const queryString = require('querystring')
+
+module.exports = function(request) {
+  request.url = urlParser.parse(request.url)
+  request.url.query = queryString.parse(request.url.query)
+  return Promise.resolve(request)
+}
