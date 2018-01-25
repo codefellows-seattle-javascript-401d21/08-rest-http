8008909bf3454ceb028533d3eeecf4a4de5fb8d1
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-25 07:45:23 -0800

    Modified the error checking a bit

--- lab-steve/lib/storage.js
+++ lab-steve/lib/storage.js
@@ -9,53 +9,83 @@ storage.create = function(schema, item) {
 
   return new Promise((resolve, reject) => {
     debug('Adding a new item to memory.');
-    // Validate first
-    if(!schema || !item)
+    if (!schema || !item)
       return reject(new Error('Cannot create a new item; Schema and Item required'));
 
-    // If the schema doesn't exist already, create it
     if(!memory[schema]) memory[schema] = {};
 
-    // Add the new item
     memory[schema][item._id] = item;
 
-    // Return the item
     return resolve(memory[schema][item._id]);
   });
 };
 
-storage.fetchOne = function (schema, _id) {
+storage.fetchOne = (schema, _id) => {
   debug(`Fetching single ${schema} with id "${_id}".`);
 
   return new Promise((resolve, reject) => {
-    // Check the schema
-    if (!schema || !memory[schema])
-      return reject(new Error('Cannot fetch One; Schema required'));
-    // Check the id
+    if (!schema)
+      return reject(new Error('Error: No schema provided!'));
+    if (!_id)
+      return reject(new Error('Error: No _id provided!'));
+    if (!memory[schema])
+      return reject(new Error(`Error: Schema ${schema} has no elements!`))
     if (!memory[schema][_id])
-      return reject(new Error('Cannot fetch One; Id required'));
+      return reject(new Error(`Error: Schema ${schema}, element id ${_id} does not exist`));
 
     return resolve(memory[schema][_id]);
   });
 };
 
-storage.fetchAll = function (schema) {
+storage.fetchAll = (schema) => {
   debug(`Fetching all ${schema}'s from memory.`);
 
   return new Promise((resolve, reject) => {
-    // Check the schema
-    if(!schema || !memory[schema])
-      return reject(new Error('Cannot fetch All; Schema required'));
+    if (!schema)
+      return reject(new Error('Error: No schema provided!'));
+    if (!memory[schema])
+      return reject(new Error(`Error: Schema ${schema} has no elements!`));
 
     return resolve(memory[schema]);
   });
 };
 
-storage.update = function() {
-  // TODO: Implement me
+storage.update = (schema, ele) => {
+  debug(`Updating schema ${schema} element`);
+
+  return new Promise((resolve, reject) => {
+    if (!schema)
+      return reject(new Error('Error: No schema provided!'));
+    if (!ele)
+      return reject(new Error('Error: No element to update provided!'));
+    if (!memory[schema])
+      return reject(new Error('Error: Schema does not exist!'));
+    if (!memory[schema][ele._id])
+      return reject(new Error('Error: Element does not exist!'));
+
+    // update it!
+    memory[schema][ele._id] = ele;
+
+    return resolve(ele);
+  });
 };
 
-storage.delete = function() {
-  // TODO: Implement me
+storage.delete = (schema, _id) => {
+  debug(`Deleting element id=${_id} from ${schema} schema`);
+
+  return new Promise((resolve, reject) => {
+    if (!schema)
+      return reject(new Error('Error: No schema provided!'));
+    if (!_id)
+      return reject(new Error('Error: No element _id to delete provided!'));
+    if (!memory[schema])
+      return reject(new Error('Error: Schema does not exist!'));
+    if (!memory[schema][_id])
+      return reject(new Error('Error: Element does not exist!'));
+
+    delete memory[schema][_id];
+
+    return resolve(_id);
+  })
 };
 
