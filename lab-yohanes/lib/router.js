42c5adffaa891487aaacfcf6e8aeee9c54caaa6d
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-24 21:12:39 -0800

    last commit before trial and error

--- lab-yohanes/lib/router.js
+++ lab-yohanes/lib/router.js
@@ -8,7 +8,7 @@ const urlParser = require('./url-parser')
 
 const Router = module.exports = function () {
   this.routes = {
-    GET: {
+    GET: { 
 
     },
     POST: {
@@ -31,10 +31,39 @@ Router.prototype.post = function(ednpoint, callback) {
   this.routes.POST[endpoint] = callback
 }
 
-Router.prototype.post = function (endpoint, callback) {
+Router.prototype.put = function (endpoint, callback) {
   this.routes.PUT[endpoint] = callback
 }
 
-Router.prototype.post = function (endpoint, callback) {
+Router.prototype.delete = function (endpoint, callback) {
   this.routes.DELETE[endpoint] = callback
+}
+
+Router.prototype.route = function () {
+  return (req, res) => {
+    Promise.all([ //commit to promises at url/body parser requests
+      urlParser(req),
+      bodyParser(req),
+    ])
+    .then(() => {
+      debug('Successfully parsed the Body and URL')
+
+      if(typeof this.routes[req.method][req.url.pathname] === 'function') {
+        this.routes[req.mehtod][req.url.pathname](req, res)
+        return
+      }
+
+      res.writeHead(404, {'Content-Type': 'text/plain'})
+      res.write('Not Found')
+      res.end()
+      return
+    })
+    .catch(error => {
+      debug( `There was an error parsing the URL or Body: ${error}`)
+
+      res.write(400, {'Content-Type': 'text/plain'})
+      res.end()
+      return
+    })
+  }
 }
