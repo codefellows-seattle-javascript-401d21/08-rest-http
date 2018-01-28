24e6924af0af3020ba399e8f9e418d9f4112aeb1
Mitchell M <mitchellm1618@gmail.com>
2018-01-28 13:59:03 -0800

    commit changes

--- lab-mitchell/lib/server.js
+++ lab-mitchell/lib/server.js
@@ -1,5 +1,12 @@
 'use strict';
 
+//example of promisifying things
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'}); //can ignore callbacks for fs.readFile, pass it .then .catch instead 
+//suffix: 'Prom' allws us to distinguish between the original version and promisified version. both versions available for use
+// fs.readFile(path, callback) //normal version
+// fs.readFileProm(path).then().catch(); //suffix version
+
 const http = require('http');
 const Router = require('./router');
 
