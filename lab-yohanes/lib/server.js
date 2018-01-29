9c059595fee4d1278fe3d2b83072e36a732c49e6
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-28 21:52:50 -0800

    SUCCERSSFULLY completed tests and fixed ALL typos

--- lab-yohanes/lib/server.js
+++ lab-yohanes/lib/server.js
@@ -1,17 +1,17 @@
 'use strict';
 
 //app dependencies
-const http = require('http')
-const Router = require('./router')
+const http = require('http');
+const Router = require('./router');
 
 //Router Setup
-const router = new Router()
-require('../route/route-note')('router')
+const router = new Router();
+require('../route/route-note')(router);
 
 //App setup
-const app = http.createServer(router.routes())
+const app = http.createServer(router.route());
 
 //Server Controls
-const server = modules.exports = {}
-server.start = (port, cb) => app.listen(port, cb)
-server.stop = (cb) => app.close(cb) //I think this is actively listening to call backs on the port when server stops and server stops
+const server = module.exports = {};
+server.start = (port, cb) => app.listen(port, cb);
+server.stop = (cb) => app.close(cb); //I think this is actively listening to call backs on the port when server stops and server stops
--- lab-yohanes/lib/url-parser.js
+++ lab-yohanes/lib/url-parser.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const urlParser = require('url')
-const queryString = require('querystring') //url extension where we will be pulling from database
-const debug = require('debug')('http:url-parser')
+const urlParser = require('url');
+const queryString = require('querystring');
+const debug = require('debug')('http:url-parser');
 
-module.exports = function(request) {
-  request.url = urlParser.parse(request.url)
-  request.url.query = queryString.parse(reauest.uel.query)
-
-  return Promise.resolve(request)
-}
+module.exports = function (request) {
+  request.url = urlParser.parse(request.url);
+  request.url.query = queryString.parse(request.url.query);
+  return Promise.resolve(request);
+};
